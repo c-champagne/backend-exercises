5a1bd802d1906af57af50cbd4e96e67b8418af8e
c-champagne <cassielchampagne@gmail.com>
2020-04-07 18:19:17 -0400

    Progress?

--- socialapp.js
+++ socialapp.js
@@ -1,34 +1,39 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-let recentshouts = [
+
+// support parsing of application/json type post data
+const parseData = bodyParser.json();
+let recentShouts = [
+
     {name: "Russell",
     shout: "I'm watching a good movie!"
     },
     {name: "Indiana",
     shout: "Why did it have to be snakes?"
-    }
+    } 
 ];
 
-app.post("/shout", function (req, res, next) {
+app.post("/shout", parseData, function (req, res, next) {
     /* let name = prompt("Name:", "User") ;
     let shout = prompt("What's worth shouting about?", "Test Shout"); */
-    recentshouts.pop(name);
-    recentshouts.pop(shout);
-    res.send(recentshouts[0].name +" shouts "+ recentshouts[0].message)
+    let newShout = {};
+    newShout["name"] = req.body.name;
+    newShout["shout"] = req.body.shout;
+    recentShouts.push(newShout);
+    res.send("You shouted: " + newShout["shout"]);
 })
 
 app.get("/newshouts", function (req, res, next) {
-    res.send(recentshouts);
+    res.send(recentShouts);
 })
 
-app.post("/posturl",function(req,res,next){
+/* app.post("/posturl",function(req,res,next){
     console.log(req.body);
     res.send("response");
-})
+}) */
 
-// support parsing of application/json type post data
-app.use(bodyParser.json());
+
 //support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
 
